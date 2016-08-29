8d010f7ef265352ac81e9b301167681f22cb0df3
lbingd17 <lbingd17@mail.ustc.edu.cn>
2016-08-29 09:32:32 +0800

    Create a009LastKNode.java

---
+++ cc150/a009LastKNode.java
@@ -0,0 +1,29 @@
+/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+public class Solution {
+    public ListNode FindKthToTail(ListNode list,int k) {
+if (list == null)   return list;
+ 
+        ListNode node = list;
+        int count = 0;
+        while (node != null) {
+            count++;
+            node = node.next;
+        }
+        if (count < k)  return null;
+ 
+        ListNode p = list;
+        for (int i = 0; i < count - k; i++) {
+            p = p.next;
+        }
+         
+        return p;
+    }
+}
