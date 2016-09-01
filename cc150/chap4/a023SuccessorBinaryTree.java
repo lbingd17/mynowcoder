0ae5e3ee4a0c0f048b41ecbda17d3c33b31654b6
lbingd17 <lbingd17@mail.ustc.edu.cn>
2016-09-01 11:28:31 +0800

    Update a023SuccessorBinaryTree.java

--- cc150/chap4/a023SuccessorBinaryTree.java
+++ cc150/chap4/a023SuccessorBinaryTree.java
@@ -1,3 +1,38 @@
+ 
+import java.util.*;
+ 
+/*
+中序遍历非递归算法
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}*/
+public class Successor {
+    public int findSucc(TreeNode root, int p) {
+        boolean isFound = false;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode cur = root;
+        while(cur!=null||!stack.isEmpty()){
+            while(cur!=null){
+                stack.add(cur);
+                cur = cur.left;
+            }
+            if(!stack.isEmpty()){
+                TreeNode q = stack.pop();
+                if(isFound) return q.val;
+                else if(q.val==p) isFound = true;
+                cur = q.right;
+            }
+        }
+        return -1;
+    }      
+}
+
+///////解法2
 import java.util.*;
  
 /*
@@ -9,6 +44,7 @@ public class TreeNode {
         this.val = val;
     }
 }*/
+
 public class Successor {
         ArrayList<TreeNode> ls=new ArrayList<TreeNode>();
     public int findSucc(TreeNode root, int p) {
