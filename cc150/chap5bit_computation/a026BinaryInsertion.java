388e590a87fab555eb5f038eb36c726d15abced5
lbingd17 <lbingd17@mail.ustc.edu.cn>
2016-10-10 15:19:57 +0800

    Update a026BinaryInsertion.java

--- cc150/chap5bit_computation/a026BinaryInsertion.java
+++ cc150/chap5bit_computation/a026BinaryInsertion.java
@@ -1,1 +1,10 @@
-todo
+import java.util.*;
+
+public class BinInsert {
+    public int binInsert(int n, int m, int j, int i) {
+		int put = m << j;//signed left shift
+		int res = n|put;
+    	return res;
+    }
+}
+//http://www.nowcoder.com/questionTerminal/30c1674ad5694b3f8f0bc2de6f005490
